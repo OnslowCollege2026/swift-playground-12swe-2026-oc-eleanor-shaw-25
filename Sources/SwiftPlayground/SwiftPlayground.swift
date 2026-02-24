ddc76c9f2282bb50b71cda2c8435528d1a6631d2
oc-eleanor-shaw-25 <eleanor.shaw@student.onslow.school.nz>
2026-02-24 00:13:15 +0000

    TicTacToe

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,110 +2,122 @@
 // https://docs.swift.org/swift-book
 
 
-func readNumber(prompt: String) -> Double {
-    var returnValue = -1.0
-
-    print(prompt)
-    guard let input = readLine(), let number = Double(input), number > 0 else {
-        print("Invalid number")
-        return readNumber(prompt: prompt)
+func printBoard(board: [[String]]) {
+    print("+---+---+---+")
+    board.forEach { line in
+        print("| \(line[0]) | \(line[1]) | \(line[2]) |")
+        print("+---+---+---+")
     }
-    returnValue = number
-    return returnValue
-    
+
 }
 
+func askForMove(board: [[String]]) -> [Int] {
+    while true{
+    print("Please enter the row number:")
+    if let userInput = readLine(), var rowNumber = Int(userInput) {
+        rowNumber -= 1 
+
+        print("Please enter the column number:")
+        if let userInput = readLine(), var columnNumber = Int(userInput) {
+        columnNumber -= 1
+         if board[rowNumber][columnNumber] == "." {
+        return [rowNumber,columnNumber] 
+
+    } else {
+        print("Invalid number")
+    }
+
+    } else {
+        print("Invalid number")
+    }
+    
+    
+   
+    }
+    }
+}
 @main
 struct SwiftPlayground {
     static func main() {
 
-        let minimumLength = 0.0
-        let minimumWidth = 0.0
-        let minimumHeight = 0.0
-        let oversizedFuriture = 2.0
+var playerMoves = 0
 
-        var thing = true
+var board = [
+    [".",".","."], //row 0
+    [".",".","."], //row 1
+    [".",".","."], //row 2
+]
+printBoard(board: board)
 
-        while thing == true {
-            print("What is the length of your room?")
+var player = "x"
 
-            
-            var thing2 = true
+while playerMoves < 9 {
+//Ask for users move
+let position = askForMove(board: board)
+board[position[0]][position[1]] = player
+printBoard(board: board)
+playerMoves += 1
 
-            if let input = readLine(), let roomLength = Double(input), roomLength > minimumLength {
-                while thing2 == true {
-                    thing = false
-
-                    print("What is the width of your room?")
-
-                    if let input = readLine(), let roomWidth = Double(input), roomWidth > minimumWidth {
-                        let roomArea = roomWidth * roomLength
-                        
-                        thing2 = false
-
-                        var thing3 = true
-                        while thing3 == true {
-
-                            print("What is the height of your room?")
-                            if let input = readLine(), let roomHeight = Double(input), roomHeight > minimumHeight {
-                                let roomVolume = roomHeight * roomLength * roomWidth
-                                
-                                thing3 = false
-
-                                let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-
-
-                                for (index, furniture) in furnitureVolumes.enumerated() {
-                                    print("Item \(index + 1): \(furniture)")
-                                    if furniture > oversizedFuriture {
-                                        print("Oversized item detected")
-                                    }
-                                }
-                                let totalFurnitureVolume = furnitureVolumes.reduce(0,+)
-                                let usableVolume = roomVolume - totalFurnitureVolume
-                                
-                                print("\(roomArea)m²")
-                                print("\(roomVolume)m³")
-                                print("\(totalFurnitureVolume)m³")
-                                print("Usable volume: \(usableVolume)m³")
-
-                                if usableVolume > 60.0 {
-                                    print("Usable volume is fine.")
-                                } else {
-                                    print("You don't have enough space boy!")
-                                }
+if player == "x"{
+    player = "o"
+} else {
+    player = "x"
+}
 
 
 
 
-                            } else {
-                            print("invalid number")
-                            }
+}
+// //First move:0 in the middle.
+// board[1][1] = "O"
+// printBoard(board: board)
 
-                        }
-                    } else {
-                        print("invalid number")
-                    }
+// //Second move : x in top left.
+// board[0][0] = "X"
+// printBoard(board: board)
 
-                }
-            } else {
-                    print("invalid number")
-            }
-        }
+// //Third move : o in top-right.
+// board[0][2] = "O"
+// printBoard(board: board)
+
+// //Fourth move : x in bottom-left.
+// board[2][0] = "X"
+// printBoard(board: board)
+
+// //Fifth move : o in middle-left.
+// board[1][0] = "O"
+// printBoard(board: board)
+
+// //Sixth move : x in middle-right.
+// board[1][2] = "X"
+// printBoard(board: board)
+
+// //Seventh move : o in bottom-middle.
+// board[2][1] = "O"
+// printBoard(board: board)
+
+// //Eighth move : x in bottom-right.
+// board[2][2] = "X"
+// printBoard(board: board)
 
 
-
-
-
-
-
-
-
-
-
-
+// //Ninth move : o in top-middle.
+// board[0][1] = "O"
+// printBoard(board: board)
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
